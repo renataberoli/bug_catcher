8eee8fee4628aaf655c9c92e47893d4948463a30
renata <renata.olive19@gmail.com>
2022-03-04 10:13:57 +0100

    fix authentication

--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from .models import Issue
 from .forms import IssueForm
 from django.utils import timezone
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def issue_list(request):
     search_argument = request.GET.get("data")
     priority = request.GET.get("priority")
--- bug_catcher/settings.py
+++ bug_catcher/settings.py
@@ -120,6 +120,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Login & Logout URLs
-LOGIN_URL = '/login/'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
