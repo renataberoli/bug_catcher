0b7f835f2e6522e09bef553b94d7e3c0868e4e0b
renata <renata.olive19@gmail.com>
2022-03-07 22:16:37 +0100

    creating tests

---
+++ app/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class BcTestCase(TestCase):
+    client = Client()
+
+    def setUp(self):
+        user = User.objects.create(username='renataberoli')
+        user.set_password('123and4')
+        user.save()
+
+        return user
+
+    def test_login_failed(self):
+        response = self.client.post('/accounts/login/', {'username': 'beroli', 'password': '1234'})
+        self.assertIn(b"correct username", response.content)
+
+    def test_login_success(self):
+        response = self.client.post('/accounts/login/', {'username': 'renataberoli', 'password': '123and4'})
+        self.assertRedirects(response, '/')
+
+
--- app/tests/test_views.py
+++
@@ -1,6 +0,0 @@
-from django.test import SimpleTestCase
-
-
-class TestUrls(SimpleTestCase):
-    def test_url(self):
-        assert 1 == 2
