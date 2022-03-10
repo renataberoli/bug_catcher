a6fd12159bc163f7d685169e37fb779e9069038b
renata <renata.olive19@gmail.com>
2022-03-10 10:28:51 +0100

    creating tests

--- app/models.py
+++ app/models.py
@@ -45,7 +45,7 @@ class Issue(models.Model):
     description = models.TextField(max_length=5000, blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author')
     assignee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, related_name='assignee')
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
     attached_file = models.ForeignKey(File, on_delete=models.CASCADE, blank=True, null=True)
     status = models.CharField(max_length=100, choices=STATUS, default='open', blank=True)
     label = models.CharField(max_length=200, choices=LABELS, blank=True)
